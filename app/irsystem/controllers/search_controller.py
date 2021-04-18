9e2952234a257172a5409e7904248af80d6fb854
Shuyan Huang <sh2429@cornell.edu>
2021-04-18 19:46:19 +0800

    basic inverted index

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -5,16 +5,25 @@ from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 project_name = "Zoogle Search"
 net_id = "Thomas Lee (tcl59), Joseph Choi (jc2493), Ray Wei (ryw23), Joyce Huang (sh2429), Clara Song (cs2274)"
 
+combined_inv_idx = load_idx()
+combined_idf_dict = load_idf()
+combined_doc_norms = load_norms()
+combined_breeds_info = load_breed()
+
 @irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
 	if not query:
 		data = []
-		output_message = ''
+		output_message = ""
 	else:
 		output_message = "Your search: " + query
-		data = range(5)
+		results = index_search(query, combined_inv_idx, combined_idf_dict, combined_doc_norms)
+		data = results
+		breeds = list(combined_breeds_info.keys())
+		for i in range(len(results)):
+			score = results[i][0]
+			msg_id = results[i][1]
+			data[i] = str(i) + ": " + str(breeds[msg_id]) + "(" + str(score) + ")"
+		data = data[:10]
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
-
-
-
